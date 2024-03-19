a8ffc1173624bf33edc3ff9c4c5733d62430ad68
gorrus39 <gorrus100@gmail.com>
2024-03-19 10:23:51 +0200

    try Render

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require_relative 'boot'
 
 require 'rails/all'
 require 'dotenv'
-Dotenv.load
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,6 +11,8 @@ Bundler.require(*Rails.groups)
 
 module RailsProject64
   class Application < Rails::Application
+    Dotenv.load(Rails.root.join('.public_env').to_s)
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
